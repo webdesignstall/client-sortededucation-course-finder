d2492dfb950977d62a4494f629b8c3a86985ffe3
Alamgir Hossen <96404769+developeralamgirbd@users.noreply.github.com>
2023-09-05 15:40:20 +0600

    Initial layouts setup

---
+++ src/pages/dashboard/index.js
@@ -0,0 +1,17 @@
+import DashboardLayout from "@/components/Layouts/DashboardLayout";
+
+const Dashboard = () => {
+    return (
+        <>
+            Admin Dashboard
+        </>
+    );
+};
+
+export default Dashboard;
+
+Dashboard.getLayout = function getLayout(page) {
+    return (
+            <DashboardLayout>{page}</DashboardLayout>
+    );
+};
