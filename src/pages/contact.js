a3713aa0473acae9aed5754b39cfab38aa06db08
Sahidul Islam <jonyahmed19@gmail.com>
2023-09-10 19:39:27 +0600

    [Done] Home Page draft

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -1,9 +1,25 @@
+/* eslint-disable react/no-unescaped-entities */
+
 import RootLayout from "@/components/Layouts/RootLayout";
 import Head from "next/head";
-import {Col, Row} from "antd";
-import Image from "next/image";
+import {Button, Form, Input, InputNumber, Col, Row} from "antd";
 
+/* eslint-disable no-template-curly-in-string */
+const validateMessages = {
+    required: '${label} is required!',
+    types: {
+        email: '${label} is not a valid email!',
+        number: '${label} is not a valid number!',
+    },
+    number: {
+        range: '${label} must be between ${min} and ${max}',
+    },
+};
+/* eslint-enable no-template-curly-in-string */
 
+const onFinish = (values) => {
+    console.log(values);
+};
 const ContactUs = () => {
 
     return (
@@ -15,19 +31,99 @@ const ContactUs = () => {
                 <div className='container page-space contact'>
                     <Row>
                         <Col xs={24} sm={24} md={11}>
-                            <h2>WHO WE ARE</h2>
-                            <p>We are a dedicated team committed to simplifying the university application process for
-                                you. Our mission is to provide personalized guidance and unwavering support, helping you
-                                navigate the intricate admissions journey and significantly increasing your chances of
-                                being accepted into your dream universities. With our comprehensive service, you can
-                                approach the application process with confidence and peace of mind, knowing that we are
-                                here to assist you every step of the way. Let us be your trusted partners in achieving
-                                your academic aspirations and securing a bright future ahead.</p>
+                            <h2>CONTACT US</h2>
+                            <div className='contact-conent'>
+
+
+                                <p className='bold'>Contact Us for a Journey Towards Your Educational Success!</p>
+                                <p>We're thrilled to be part of your academic journey and are here to provide all the
+                                    support you
+                                    need. Whether you have questions, need personalized guidance, or want to discuss our
+                                    services in detail, don't hesitate to reach out.
+                                </p>
+                                <p><span className='bold'>Email us at:</span> abdulla@sortededucation.com
+                                </p>
+                                <p>
+                                   <span className='bold'>Call or WhatsApp:</span> +973 36992110
+
+                                </p>
+                                <p>Our dedicated team is ready to listen, understand your aspirations, and craft a
+                                    tailored
+                                    plan to
+                                    achieve your educational goals. Your dreams matter to us, and we're committed to
+                                    making
+                                    your
+                                    path to university success as smooth as possible.
+                                </p>
+                                <p>Let's work together towards a brighter future. Contact us today, and let the journey
+                                    begin!</p>
+
+                            </div>
                         </Col>
 
                         <Col xs={0} sm={0} md={2}></Col>
                         <Col xs={24} sm={24} md={11} style={{display: 'flex', alignItems: 'center'}}>
-                            <Image layout="intrinsic" width={700} height={600} src={'/static/image-asset.jpeg'} alt={"Who We Are"} />
+                            <Form
+                                name="contact"
+                                onFinish={onFinish}
+                                validateMessages={validateMessages}
+                                layout={'vertical'}
+                                style={{width: '100%'}}
+                            >
+                                <Form.Item
+                                    name={'firstName'}
+                                    label="First Name"
+                                    rules={[
+                                        {
+                                            required: true,
+                                        },
+                                    ]}
+                                >
+                                    <Input size={'large'}/>
+                                </Form.Item>
+                                <Form.Item
+                                    name={'lastName'}
+                                    label="Last Name"
+                                    rules={[
+                                        {
+                                            required: true,
+                                        },
+                                    ]}
+                                >
+                                    <Input size={'large'}/>
+                                </Form.Item>
+                                <Form.Item
+                                    name={'email'}
+                                    label="Email"
+                                    rules={[
+                                        {
+                                            required: true,
+                                            type: 'email',
+                                        },
+                                    ]}
+                                >
+                                    <Input size={'large'}/>
+                                </Form.Item>
+
+                                <Form.Item
+                                    name={'message'}
+                                    label="Message"
+                                    rules={[
+                                        {
+                                            required: true
+                                        }
+                                    ]}
+                                >
+                                    <Input.TextArea rows={4} />
+                                </Form.Item>
+
+                                <Form.Item
+                                >
+                                    <Button size={'large'} type="primary" htmlType="submit">
+                                        Submit
+                                    </Button>
+                                </Form.Item>
+                            </Form>
                         </Col>
                     </Row>
                 </div>
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,67 @@
 import Head from 'next/head'
-import UnderConstruction from "@/components/UnderConstruction";
 import RootLayout from "@/components/Layouts/RootLayout";
+import {Button, Col, Form, Input, Row, Select} from "antd";
+
+
+/* eslint-disable no-template-curly-in-string */
+const validateMessages = {
+    required: '${label} is required!',
+    types: {
+        email: '${label} is not a valid email!',
+        number: '${label} is not a valid number!',
+    },
+    number: {
+        range: '${label} must be between ${min} and ${max}',
+    },
+};
+/* eslint-enable no-template-curly-in-string */
+
+const onFinish = (values) => {
+    console.log(values);
+};
+
+const subject = [
+    {
+        label: 'Agriculture',
+        value: 'agriculture'
+    },
+    {
+        label: "Law",
+        value: 'law'
+    },
+    {
+        label: "Medicine",
+        value: "medicine"
+    }
+];
+const qualifacation = [
+    {
+        label: 'Foundation',
+        value: 'foundation'
+    },
+    {
+        label: "Diploma",
+        value: "diploma"
+    },
+    {
+        label: "Ph.D",
+        value: 'phd'
+    }
+];
+const country = [
+    {
+        label: 'United Kingdom',
+        value: 'unitedkingdom'
+    },
+    {
+        label: "United States",
+        value: "unitedstates"
+    },
+    {
+        label: "Canada",
+        value: "canada"
+    }
+];
 
 export default function Home() {
     return (
@@ -9,7 +70,60 @@ export default function Home() {
                 <title>Enroll and Excel</title>
             </Head>
             <main>
-                <UnderConstruction/>
+                <div className='container page-space home'>
+                    <Row>
+                        <Col md={24}>
+                            <h2 className='search-label'>UNIVERSITY SEARCH</h2>
+                            <div className='search-field'>
+                                <Form
+                                    name="contact"
+                                    onFinish={onFinish}
+                                    validateMessages={validateMessages}
+                                >
+                                    <Form.Item
+                                        name={'subject'}
+                                        label="Select a Subject:"
+                                        rules={[
+                                            {
+                                                required: true,
+                                            },
+                                        ]}
+                                    >
+                                        <Select size={'large'} placeholder={'Select a Subject'} showSearch={true} options={subject} />
+                                    </Form.Item>
+                                    <Form.Item
+                                        name={'qaulification'}
+                                        label="Select a Qualification:"
+                                        rules={[
+                                            {
+                                                required: true,
+                                            },
+                                        ]}
+                                    >
+                                        <Select size={'large'} placeholder={'Select a Qualification'} showSearch={true} options={qualifacation} />
+                                    </Form.Item>
+                                    <Form.Item
+                                        name={'location'}
+                                        label="Select a Location:"
+                                        rules={[
+                                            {
+                                                required: true,
+                                            },
+                                        ]}
+                                    >
+                                        <Select size={'large'} placeholder={'Select a Location'} showSearch={true} options={country} />
+                                    </Form.Item>
+                                    <Form.Item
+                                    >
+                                        <Button size={'large'} type="primary" htmlType="submit">
+                                            Search
+                                        </Button>
+                                    </Form.Item>
+                                </Form>
+                            </div>
+                        </Col>
+                    </Row>
+                </div>
             </main>
         </>
     )
