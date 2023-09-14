2a6e76530a05c55fb109ec3e2596fbd085bc1652
developeralamgirbd <developer.alamgirbd@gmail.com>
2023-09-14 21:17:45 +0600

    [Done] course

---
+++ src/pages/dashboard/course/create/[id].js
@@ -0,0 +1,23 @@
+import DashboardLayout from "@/components/Layouts/DashboardLayout";
+import React from "react";
+import {useSearchParams} from "next/navigation";
+import CourseForm from "@/components/course/CourseForm";
+
+const CourseUpdate = () => {
+    const params = useSearchParams();
+    const courseId = params.get('id')
+
+    return (
+        <>
+           <CourseForm courseId={courseId}/>
+        </>
+    );
+};
+
+export default CourseUpdate;
+
+CourseUpdate.getLayout = function getLayout(page) {
+    return (
+        <DashboardLayout>{page}</DashboardLayout>
+    );
+};
