73dac4b39af217ac097b67dabf7aaa58ef6dccc1
developeralamgirbd <developer.alamgirbd@gmail.com>
2023-09-25 13:04:51 +0600

    [Done] countries & course view table

--- src/pages/universities.js
+++ src/pages/universities.js
@@ -1,40 +1,8 @@
 import RootLayout from "@/components/Layouts/RootLayout";
 import Head from "next/head";
-import {Col, Row, Table} from "antd";
-import Image from "next/image";
+import {Col, Row, Skeleton, Table} from "antd";
 import handleRequest from "@/utilities/handleRequest";
-import university from "@/pages/dashboard/university";
-import {useRouter} from "next/router";
-
-const data = [
-    {
-        key: '1',
-        university: 'https://ibec.or.id/wp-content/uploads/2018/07/uni_logo_reading_1280_510.jpg',
-        qualification: 'BA Honors',
-        details: '<p>Tuition Fees: £21,615.54 GBP / Year </p><p>Number of Students: 19,390</p><p>Distance from Central</p><p> London: 30 min Via Paddington station</p>',
-        location: 'New York No. 1 Lake Park',
-    },
-    {
-        key: '2',
-        university: 'https://ibec.or.id/wp-content/uploads/2018/07/uni_logo_reading_1280_510.jpg',
-        qualification: 'MBA Honors',
-        details: '<p>Tuition Fees: £21,615.54 GBP / Year </p><p>Number of Students: 19,390</p><p>Distance from Central</p><p> London: 30 min Via Paddington station</p>',
-        location: 'New York No. 1 Lake Park',
-    },
-    {
-        key: '3',
-        university: 'https://ibec.or.id/wp-content/uploads/2018/07/uni_logo_reading_1280_510.jpg',
-        qualification: 'CSE Computer',
-        details: '<p>Tuition Fees: £21,615.54 GBP / Year </p><p>Number of Students: 19,390</p><p>Distance from Central</p><p> London: 30 min Via Paddington station</p>',
-        location: 'New York No. 1 Lake Park',
-    }, {
-        key: '4',
-        university: 'https://ibec.or.id/wp-content/uploads/2018/07/uni_logo_reading_1280_510.jpg',
-        qualification: 'BSC Math',
-        details: '<p>Tuition Fees: £21,615.54 GBP / Year </p><p>Number of Students: 19,390</p><p>Distance from Central</p><p> London: 30 min Via Paddington station</p>',
-        location: 'New York No. 1 Lake Park',
-    },
-];
+import Image from "next/image";
 
 
 const columns = [
@@ -42,20 +10,18 @@ const columns = [
         title: 'University',
         dataIndex: 'university',
         key: 'university',
-        render: (url) => <img width={300} src={url} alt={university?.name}/>
+        render: (university) => <Image width={300} height={120} src={university?.logo?.secure_url} alt={university?.name}/>
     },
     {
         title: 'Qualification',
-        dataIndex: 'name',
-        key: 'name',
-        // responsive: ['md'],
-        render: (text) => <p>{text}</p>
+        dataIndex: 'qualification',
+        key: 'qualification',
+        render: (qualification) => <p>{qualification?.name}</p>
     },
     {
         title: 'Details',
         dataIndex: 'distance',
         key: 'distance',
-        // responsive: ['lg'],
         render: (text, items) => {
             return (
                 // <div dangerouslySetInnerHTML={{__html: text}}/>
@@ -77,12 +43,12 @@ const columns = [
                 </>
             )
         },
-    }, {
+    },
+    {
         title: 'Location',
-        dataIndex: 'country',
+        dataIndex: 'university',
         key: 'country',
-        // responsive: ['lg'],
-        render: (text) => <p>{text}</p>
+        render: (university) => <p>{university?.country}</p>
     },
 ];
 
@@ -97,7 +63,8 @@ const Universities = ({courses}) => {
                 <div className='container page-space search-result'>
                     <Row>
                         <Col xs={24} sm={24} md={24}>
-                            <Table className='search-table' columns={columns} dataSource={courses}/>
+                            {courses?.length ? <Table className='search-table' columns={columns} dataSource={courses}/> : <Skeleton></Skeleton>}
+
                         </Col>
 
                     </Row>
@@ -121,18 +88,14 @@ export async function getServerSideProps(context) {
 
     const {subjectId, qualificationId, universityId} = context.query || {};
 
-    // console.log('universityId', universityId)
-
     let url = '/courses';
     if (subjectId) {
         url = `/courses?subjectId=${subjectId}&qualificationId=${qualificationId}&universityId=${universityId}`
     } else {
         url = '/courses'
     }
-
-    console.log('url',url)
     const responseCourses = await handleRequest('get', url);
-    console.log('responseCourses', responseCourses);
+
     // Pass the data to the component as props
     return {
         props: {
