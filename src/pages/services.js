843c4ad667fe144d38dc533a96ca138a5a8a623d
Sahidul Islam <jonyahmed19@gmail.com>
2023-09-26 18:50:03 +0600

    fix

--- src/pages/services.js
+++ src/pages/services.js
@@ -4,51 +4,6 @@ import Head from "next/head";
 import {Col, Collapse, Row} from "antd";
 import ServicesImage from "../../public/images/services.jpeg";
 import handleRequest from "@/utilities/handleRequest";
-import {useEffect} from "react";
-
-const items = [
-    {
-        key: "1",
-        label: <h4>Personalized Journey</h4>,
-        children: (
-            <p>
-                Join us on an exhilarating journey to your dream university, where we'll
-                walk alongside you, understanding your unique passions and ambitions.
-                Through genuine, heartfelt conversations, we'll uncover the perfect fit
-                that aligns with your future aspirations, creating a path tailored just
-                for you.
-            </p>
-        ),
-    },
-    {
-        key: "2",
-        label: <h4>Empowering Excellence</h4>,
-        children: (
-            <p>
-                Feel the thrill as we equip you with the tools and confidence to shine
-                brightly throughout the application process. From test preparation that
-                ignites your potential to interview coaching that lets your authentic
-                self sparkle, we'll be your unwavering cheerleaders, championing your
-                success every step of the way.
-            </p>
-        ),
-    },
-    {
-        key: "3",
-        label: <h4>Seamless Support</h4>,
-        children: (
-            <p>
-                With our unwavering support, knowledge, and heartfelt care, you'll find
-                the process not just seamless but transformative. Leave the stress
-                behind and focus on your dreams as we handle the details. Embrace the
-                empowering alliance we create, for together, we'll pave the way to a
-                future brimming with endless possibilities and the realization of your
-                true potential.
-            </p>
-        ),
-    },
-];
-
 const Services = ({services, faqs}) => {
     const ImageStyle = {
         backgroundImage: `url('${ServicesImage.src}')`,
