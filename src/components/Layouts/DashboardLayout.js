aeb28682a608814038df2bcc5b785b731205ea67
developeralamgirbd <developer.alamgirbd@gmail.com>
2023-09-30 22:02:10 +0600

    [Done] show favicon in dashboard

--- src/components/Layouts/DashboardLayout.js
+++ src/components/Layouts/DashboardLayout.js
@@ -1,62 +1,83 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { Layout, theme } from "antd";
 import WithAuth from "@/middleware/WithAuth";
 import SideBarMenu from "@/components/Layouts/SideBarMenu";
 import { useSelector } from "react-redux";
+import handleRequest from "@/utilities/handleRequest";
+import Head from "next/head";
 
 const { Header, Content, Footer, Sider } = Layout;
 
 const DashboardLayout = ({ children }) => {
   const { currentUser } = useSelector((state) => state.auth);
-
+  const [general, setGeneral] = useState({});
   const {
     token: { colorBgContainer },
   } = theme.useToken();
+
+  useEffect(() => {
+    (async () => {
+      const result = await handleRequest("get", `generals`);
+      if (result.success) {
+        setGeneral(result?.data);
+      }
+    })();
+  }, []);
+
   return (
-    <Layout
-      style={{
-        minHeight: "100vh",
-      }}
-    >
-      <SideBarMenu />
-      <Layout>
-        <Header
-          style={{
-            padding: "10px 20px",
-            background: colorBgContainer,
-          }}
-        >
-          <div
+    <>
+      <Head>
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link
+          rel="icon"
+          href={general?.favicon?.secure_url || "./favicon.png"}
+        />
+      </Head>
+      <Layout
+        style={{
+          minHeight: "100vh",
+        }}
+      >
+        <SideBarMenu />
+        <Layout>
+          <Header
             style={{
-              display: "flex",
-              justifyContent: "space-between",
-              alignItems: "center",
-            }}
-          >
-            <div></div>
-            <div>
-              {currentUser?.email}
-              {/*<Avatar size="large" icon={<UserOutlined />} />*/}
-            </div>
-          </div>
-        </Header>
-        <Content
-          style={{
-            margin: "0 16px",
-          }}
-        >
-          <div
-            style={{
-              padding: 24,
-              minHeight: 360,
+              padding: "10px 20px",
               background: colorBgContainer,
             }}
           >
-            {children}
-          </div>
-        </Content>
+            <div
+              style={{
+                display: "flex",
+                justifyContent: "space-between",
+                alignItems: "center",
+              }}
+            >
+              <div></div>
+              <div>
+                {currentUser?.email}
+                {/*<Avatar size="large" icon={<UserOutlined />} />*/}
+              </div>
+            </div>
+          </Header>
+          <Content
+            style={{
+              margin: "0 16px",
+            }}
+          >
+            <div
+              style={{
+                padding: 24,
+                minHeight: 360,
+                background: colorBgContainer,
+              }}
+            >
+              {children}
+            </div>
+          </Content>
+        </Layout>
       </Layout>
-    </Layout>
+    </>
   );
 };
 
